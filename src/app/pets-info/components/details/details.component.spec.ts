f3f33119ee1b335e6bdff44ebe6a2d84c16e9e45
Patricia de Gregorio <p.degregorio@alumnos.urjc.es>
2021-04-18 09:29:10 +0200

    Details page and responsive styles

--- src/app/pets-info/components/details/details.component.spec.ts
+++ src/app/pets-info/components/details/details.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { DetailsComponent } from './details.component';
 
@@ -8,7 +10,28 @@ describe('DetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
+      declarations: [ DetailsComponent ],
+      imports: [ TranslateModule.forRoot()],
+      providers: [
+        { provide: ActivatedRoute, useValue: {
+          snapshot: {
+            data: {
+              pet: {
+                description: "I hide behind curtain when vacuum cleaner is on scratch strangers and poo on owners food but meow",
+                height: 26,
+                id: 6,
+                kind: "cat",
+                length: 50,
+                name: "Snap",
+                number_of_lives: 7,
+                photo_url: "https://cdn2.thecatapi.com/images/8k7.jpg",
+                weight: 4623
+              }
+            }
+          }
+        }
+      }
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +44,25 @@ describe('DetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should getHealth very healthy', () => {
+    component.pet.weight = 25;
+    component.pet.height = 4;
+    component.pet.length = 2.5;
+    expect(component.getHealth().label).toBe('HEALTH_LEVEL.VERY_HEALTHY');
+  });
+
+  it('should getHealth unhealthy being cat', () => {
+    component.pet.kind = 'cat';
+    component.pet.number_of_lives = 1;
+    expect(component.getHealth().label).toBe('HEALTH_LEVEL.UNHEALTHY');
+  });
+
+  it('should getHealth healthy', () => {
+    component.pet.weight = 16;
+    component.pet.height = 2;
+    component.pet.length = 2.;
+    expect(component.getHealth().label).toBe('HEALTH_LEVEL.HEALTHY');
   });
 });
--- src/app/pets-info/components/details/details.component.ts
+++ src/app/pets-info/components/details/details.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IPet } from '../../models/pet.model';
 
+enum EKind {
+  DOG = 'dog',
+  CAT = 'cat'
+};
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -16,8 +21,20 @@ export class DetailsComponent implements OnInit {
     this.pet = this.activatedRoute.snapshot.data.pet;
   }
 
-  public getHealth(): void {
+  public getHealth(): {label: string, color: string} {
+    const healthlLevel: number = this.pet.weight/(this.pet.height * this.pet.length);
+    const isUnhealthyCat: boolean = this.pet.kind === EKind.CAT && this.pet.number_of_lives === 1;
+    const unhealthy: boolean = healthlLevel < 2 || healthlLevel > 5;
+    const healthy: boolean = healthlLevel > 3 && healthlLevel < 5;
+    const veryHealthy: boolean =  healthlLevel > 2 && healthlLevel < 3;
 
+    if (isUnhealthyCat || unhealthy) {
+      return {label: 'HEALTH_LEVEL.UNHEALTHY', color: 'info__health--unhealthy'};
+    }
+    if (healthy) {
+      return {label: 'HEALTH_LEVEL.HEALTHY', color: 'info__health--healthy'};
+    }
+    return {label: 'HEALTH_LEVEL.VERY_HEALTHY', color: 'info__health--very-healthy'};
   }
 
 }
